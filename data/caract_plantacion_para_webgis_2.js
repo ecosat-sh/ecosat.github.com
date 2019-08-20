c316a528e53deffb1b6fc0ac5f76b7e02a18aefc
ecosat-sh <mb.ecosat@gmail.com>
2019-08-20 16:22:02 -0300

    primera version

---
+++ data/caract_plantacion_para_webgis_2.js
@@ -0,0 +1,9 @@
+var json_caract_plantacion_para_webgis_2 = {
+"type": "FeatureCollection",
+"name": "caract_plantacion_para_webgis_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "qc_id": "1", "id": "1", "variedad": "malbec", "plantacion": "2013-09-17", "dens_pl\/ha": "350" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -65.465917346111453, -23.739784672428645 ], [ -65.465644366317918, -23.739878925805577 ], [ -65.465929318909417, -23.740468555848615 ], [ -65.46609454352128, -23.740363343248447 ], [ -65.465917346111453, -23.739784672428645 ] ] ] ] } },
+{ "type": "Feature", "properties": { "qc_id": "2", "id": "2", "variedad": "tannat", "plantacion": "2014-08-19", "dens_pl\/ha": "320" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -65.465637182639142, -23.739889885496147 ], [ -65.465412094037447, -23.739986330733384 ], [ -65.465737754142026, -23.740639526142655 ], [ -65.465972420982084, -23.740525545971561 ], [ -65.465637182639142, -23.739889885496147 ] ] ] ] } }
+]
+}
---
+++ data/prueba_para_webgis_3.js
@@ -0,0 +1,10 @@
+var json_prueba_para_webgis_3 = {
+"type": "FeatureCollection",
+"name": "prueba_para_webgis_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "qc_id": "1", "id": "1", "herbi_date": null, "herbi_1": "glifo", "herbi_2": "2-4d", "dosis_1": 25.0, "dosis_2": 13.0 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -65.465901781474102, -23.739815359582106 ], [ -65.465676692872407, -23.739883309681929 ], [ -65.465801209971218, -23.740203332256137 ], [ -65.465995169298225, -23.740155109726757 ], [ -65.465901781474102, -23.739815359582106 ] ] ] ] } },
+{ "type": "Feature", "properties": { "qc_id": "2", "id": "2", "herbi_date": null, "herbi_1": "glifo", "herbi_2": "2-4d", "dosis_1": 5.0, "dosis_2": 7.0 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -65.465997563857798, -23.740163877460706 ], [ -65.465810788209581, -23.740212099986827 ], [ -65.465874244038787, -23.740347999737146 ], [ -65.466051441448627, -23.740286625673917 ], [ -65.465997563857798, -23.740163877460706 ] ] ] ] } },
+{ "type": "Feature", "properties": { "qc_id": "3", "id": "3", "herbi_date": null, "herbi_1": "glifo", "herbi_2": null, "dosis_1": 6.0, "dosis_2": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -65.465723386784461, -23.74005866461447 ], [ -65.465503087301954, -23.740133190389329 ], [ -65.4655868968877, -23.740319504639963 ], [ -65.465790434453055, -23.740225251581759 ], [ -65.465723386784461, -23.74005866461447 ] ] ] ] } }
+]
+}
